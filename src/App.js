e0fb7506d6e854563dcdfeb06a4aee99c9620d4d
daniel <daniel.enrriquesmercedes@gmail.com>
2023-06-21 11:11:45 +0200

    ft: i created the form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Header from "./components/Header";
 import Productos from './components/Productos'
 import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from './components/EditarProducto'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Routes>
               <Route path="/" Component={Productos}/>
               <Route path="/productos/nuevo" Component={NuevoProducto}/>
+              <Route path="/productos/editar/:id" Component={EditarProducto}/>
               
           </Routes>
 
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Productos = () => {
   return (
-    <h1>Productos</h1>
+   <Fragment>
+      <h2 className='text-center my-5'>
+
+        <table className='table table-striped'>
+            <thead className='bg-primary table-dark'>
+
+              <tr>
+                <th scope='col'>Nombre</th>
+                <th scope='col'>Precio</th>
+                <th scope='col'>Acciones</th>
+
+              </tr>
+
+            </thead>
+            <tbody>
+              
+            </tbody>
+        </table>
+
+
+
+
+      </h2>
+
+   </Fragment>
   )
 }
 
